fd651dc27894f33312961ffb2dd44b5e29b204bd
Jerehme Gayle <jerehme.gayle@gmail.com>
2018-11-01 22:17:31 -0700

    Initial commit, code cleaned up and no further revisions at this time

---
+++ main.js
@@ -0,0 +1,42 @@
+// STICKY NAV
+document.addEventListener('scroll', function() {
+  // amount of px scrolled on Y axis at all times
+  let scrollAmount = window.pageYOffset;
+  // ternary condition to add remove sticky class
+  scrollAmount > 0 ? addStickyNav() : removeStickyNav();
+});
+
+const addStickyNav = () => {
+  document.querySelector('nav').classList.add('stickynav');
+};
+
+const removeStickyNav = () => {
+  document.querySelector('nav').classList.remove('stickynav');
+};
+
+// SOCIAL SLIDER
+var slides = document.getElementsByClassName('slide');
+var index = 0;
+
+function goLeft() {
+  if (index == 0) index = slides.length - 4;
+  else index--;
+
+  slides[0].style.marginLeft = '-' + index * 480 + 'px';
+}
+
+function goRight() {
+  if (index == slides.length - 4) index = 0;
+  else index++;
+
+  slides[0].style.marginLeft = '-' + index * 480 + 'px';
+}
+
+// AUTO SCROLL FOR SOCIAL SLIDER
+setInterval(goRight, 3000);
+
+// ANIMATE ON SCROLL
+///////////////////////////////////////
+AOS.init({
+  // duration: 1500,
+});
